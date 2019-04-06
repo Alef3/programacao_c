3dd8fe01a46a943de69d28b7e9bb4054bbb2ce26
Alef3 <47638899+Alef3@users.noreply.github.com>
2019-04-06 16:18:17 -0300

    Update numeros_primos.c

--- numeros_primos.c
+++ numeros_primos.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
 int cont=0, num=2, div=1;
-
+setlocale(LC_ALL, "Portuguese");
+printf("ALGORITMO PARA NÚMEROS PRIMOS DE 2 A 100 \n");
+	
 for(num = 2; num <= 100; num++){
 	for(div = 1; div <= num; div++){
 		if(num % div == 0){
