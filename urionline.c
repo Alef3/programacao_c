a7026756cd7942fde9d8e0ef9834b6cbbd9f79d7
Alef3 <alefr9@gmail.com>
2021-03-26 19:47:08 -0300

    Update urionline.c

--- urionline.c
+++ urionline.c
@@ -8,18 +8,18 @@ int main(){
     printf("%d\n", N);
 
     int cedulas[7] = {100, 50, 20, 10, 5, 2, 1};
-    int notas;
+    int quantidade;
 
     for(int indice = 0; indice < sizeof(cedulas)/sizeof(cedulas[0]); indice++){
         //printf("%d\n", cedulas[indice]);
 
-        for(notas = 0; (cedulas[indice] * notas) <= N; notas++){
-            //printf("%d nota(s) de R$ %d,00\n", notas, cedulas[indice]);
-            //printf("%d\n", notas*cedulas[indice]);
+        for(quantidade = 0; (cedulas[indice] * quantidade) <= N; quantidade++){
+            //printf("%d nota(s) de R$ %d,00\n", quantidade, cedulas[indice]);
+            //printf("%d\n", quantidade*cedulas[indice]);
         }
-        printf("%d nota(s) de R$ %d,00\n", notas-1, cedulas[indice]);
+        printf("%d nota(s) de R$ %d,00\n", quantidade-1, cedulas[indice]);
 
-        N = N - ( cedulas[indice]*(notas-1) );
+        N = N - ( cedulas[indice]*(quantidade-1) );
 
 
     }
