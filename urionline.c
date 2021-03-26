e73c5d2fe598411bc967b9c812e0f8db86c34eed
Alef3 <alefr9@gmail.com>
2021-03-26 19:43:25 -0300

    Update urionline.c

--- urionline.c
+++ urionline.c
@@ -2,32 +2,13 @@
 
 int main(){
 
-    /*int A,B,X;
-
-    scanf("%d", &A);
-    scanf("%d", &B);
-
-    X = A + B;
-
-    printf("X = %d\n", X);*/
-
-    /*double pi = 3.14159;
-
-    double area, raio;
-
-    scanf("%lf", &raio);
-
-    area = pi * (raio*raio);
-
-    printf("A=%.4lf", area);*/
-
-    long int N;
+    int N;
     scanf("%d", &N);
 
     printf("%d\n", N);
 
     int cedulas[7] = {100, 50, 20, 10, 5, 2, 1};
-    long int notas;
+    int notas;
 
     for(int indice = 0; indice < sizeof(cedulas)/sizeof(cedulas[0]); indice++){
         //printf("%d\n", cedulas[indice]);
